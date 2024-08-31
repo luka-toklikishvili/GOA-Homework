911396fedcd1c837d004b76074ea6b897a4330fa
luka-toklikishvili <lukatbilisi90@gmail.com>
2024-08-31 17:58:26 +0400

    second commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,9 @@
+name="Luka"
+
+surname="Toklikishvili"
+
+age=14
+
+height=176,5
+
+print("me"+ " " +"mqvia"+ " " +name+" " +surname+" "+"var"+" "+str(age)+" "+"wlis"+" "+"aseve"+ " "+"var"+ " "+"sportsmeni"+ " "+"minda"+" "+"viswavlo"+" "+"programireba")
