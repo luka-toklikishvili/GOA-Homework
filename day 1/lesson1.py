b3528890c3f224502ea205d177a47487d6daf11b
luka-toklikishvili <lukatbilisi90@gmail.com>
2024-09-10 17:45:22 +0400

    second commit

--- day 1/lesson1.py
+++ day 1/lesson1.py
@@ -1,9 +1,21 @@
-name="Luka"
+name="Luka" #ეს არის str (string) ტიპის ცვლადი
 
 surname="Toklikishvili"
 
-age=14
+age=14 #ეს არის int (integer) მთელი რიცხვი
 
-height=176,5
+height=176,5 #ეს არის float ტიპის ცვლადი
+#boolean (bool) ტიპის ცვლადი
+
+knows_programing=True  #True ან False
+is_ugly=False #snakecase (უბრალოდ წერის სტილი სტანდარტულად)
+isUgly=False #javascript-ული camelcase
+#print(name+" "+surname) 
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(name+age)
+#print(type(name))
+#print (type(surname))
+#print(type(height))
+#print(type(Knows_programing))
 
 print("me"+ " " +"mqvia"+ " " +name+" " +surname+" "+"var"+" "+str(age)+" "+"wlis"+" "+"aseve"+ " "+"var"+ " "+"sportsmeni"+ " "+"minda"+" "+"viswavlo"+" "+"programireba")
