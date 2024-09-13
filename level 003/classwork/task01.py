67506fd8071753fa4ee9ea6b812b538fa1f425ef
luka-toklikishvili <lukatbilisi90@gmail.com>
2024-09-13 13:28:01 +0400

    commit

---
+++ level 003/classwork/task01.py
@@ -0,0 +1,8 @@
+name="luka "
+surname="toklikishvili"
+player=7
+speed=1.4
+player_speed=6.8
+print(name)
+print(surname)
+print(player_speed)
---
+++ level 003/homewrok/homework.py
@@ -0,0 +1,28 @@
+score=15
+print(score)
+
+health=10
+print(health)
+
+atacking_points=20
+defence_points=25
+
+print(atacking_points,defence_points)
+print(" ")
+
+math_score=9
+english_score=8
+science_score=10
+chemistry_score=7
+history_score=6
+
+print(math_score)
+print(english_score)
+print(science_score)
+print(chemistry_score)
+print(history_score)
+
+name="luka"
+surname="toklikishvili"
+age=14
+print("me"+" "+"mqvia"+" "+name+" "+surname+" " +"var"+" "+str(age)+" "+"wlis")
