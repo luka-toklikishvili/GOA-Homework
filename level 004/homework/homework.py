6ef06496cf5e63776a40c1032c03f0f18d046665
luka-toklikishvili <lukatbilisi90@gmail.com>
2024-09-17 21:00:04 +0400

    commit

---
+++ level 004/homework/homework.py
@@ -0,0 +1,38 @@
+
+
+name=input("enter you name: ")
+print(name)
+
+lastname=input("enter your surname: ")
+print(lastname) 
+
+age=input("enter your age: ")
+print(age)
+
+print(input(name+" "+lastname+" "+"is"+" "+age+" "+"years"+" "+"old"))
+
+first,second=input("enter 2 numbers: ").split()
+first=int(first)
+second=int(second)
+print(first+second)
+print(first-second)
+print(first*second)
+print(first/second)
+
+user = input("enter your name: ")
+print(user)
+
+
+math = 20 + 20
+math2 = math - 10
+math3 = math * 2
+print( math3)
+
+academy=input("enter your academy: ")
+school=input("enter your school: ")
+name=input("enter your name: ") 
+lastname=input("enter your lastname: ")
+age=input("enter you age: ")
+
+
+print(name+" "+lastname+" "+"is"+" "+age+" "+"years old "+" "+"studyes at "+" "+school+" "+"studyes programing at"+" "+academy)
