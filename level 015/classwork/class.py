006dd63c120c258a7df9894730ef62b642d31e9b
luka-toklikishvili <lukatbilisi90@gmail.com>
2024-10-26 23:23:25 +0400

    commit

---
+++ level 015/classwork/class.py
@@ -0,0 +1,10 @@
+res = ""
+giorgi_list = ["vaja", "daviti", "daviti2", True, 12]
+
+for index in range(5):
+    res += str(giorgi_list[index]) + " "
+
+
+
+
+print(res)
---
+++ level 015/homework/home.py
@@ -0,0 +1,45 @@
+#Task1
+num=int(input("enter number: "))
+
+if num<10:
+    print("ar aris atze meti")
+else:
+    print("atze metia")    
+
+#Task 2
+num1=int(input("enter number: "))
+
+if num1%2==0:
+    print("luwia")
+else:
+    print("araris luwi") 
+#task 3
+num2=int(input("enter number: "))
+    
+if num2>0:
+    print("plus number")
+else:
+    print("minus number")
+#task 4
+num3=int(input("Enter first number: "))
+num4=int(input("Enter second number: "))
+
+if num3==num4:
+   print("equals")
+else:
+    print("not equals")
+#task 5
+num5=int(input("Enter number: "))
+
+if num5>100 and num5%2==0:
+    print("asze metia da luwia") 
+else:
+ print("araris asze meti ad luwi") 
+#Task 6
+
+num6=int(input("Enter number: "))
+if num6%5==0 or num6%10==0:
+    print(num6 ,"xutis an atis jeradia")
+else:
+   print("ar aris jeradi")              
+      
