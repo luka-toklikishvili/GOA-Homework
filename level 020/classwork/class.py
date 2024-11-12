c75d88664ff06056f97e8d603c9ff2c664350914
luka-toklikishvili <lukatbilisi90@gmail.com>
2024-11-12 21:51:38 +0400

    commit

---
+++ level 020/classwork/class.py
@@ -0,0 +1,8 @@
+numb = [1, 23, 4, 5, 5, 6, 6, 1212312, 31, 4, 5, 12, 3]
+
+for i in range(len(numb)):
+    if numb[i] % 2 != 0:  
+        numb[i] = "odd"
+
+for i in range(len(numb)):
+    print(numb[i])
