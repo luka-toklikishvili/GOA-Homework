0228d2d78d606fed2077184a011dbcd6d77c47d8
luka-toklikishvili <lukaytbilisi90@gmail.com>
2025-01-01 23:53:19 +0400

    commit

---
+++ level 034/classwork/class.py
@@ -0,0 +1,8 @@
+
+favorite_movies = ("Inception", "The Matrix", "Interstellar", "The Dark Knight", "Prison Break", "Breaking Bad")
+first_movie, *remaining_movies = favorite_movies
+
+print(favorite_movies[0],favorite_movies[-1])
+
+
+
