2bd735c497b31a318906ae0fc8c9af8dd4e42e12
luka-toklikishvili <lukaytbilisi90@gmail.com>
2025-05-07 23:35:53 +0400

    commit

---
+++ level 070/classwork/script.js
@@ -0,0 +1,25 @@
+// let name = "luka"
+// let age = 15
+// let height = 175
+// let academt = "GOA"
+// let calories = 2500
+
+// console.log(name)
+// console.log(age)
+// console.log(height)
+// console.log(academt)
+// console.log(calories)
+
+// let num1 = 50
+// let num2 = 20
+
+// console.log(num1-num2)
+// console.log(num1+num2)
+// console.log(num1*num2)
+// console.log(num1/num2)
+// console.log(num1**num2)
+// function TxtChange(){
+//   let h1 =  getElementById("h-1")
+//   h1.innerText
+
+// }
