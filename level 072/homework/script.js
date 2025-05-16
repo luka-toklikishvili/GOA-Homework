b12c289bfe29570cda0080fcc0519b4b4697a5a1
luka-toklikishvili <lukaytbilisi90@gmail.com>
2025-05-16 13:46:21 +0400

    commit

---
+++ level 072/homework/script.js
@@ -0,0 +1,21 @@
+let age = 25
+console.log(age)
+
+const name = "luka"
+console.log(name)
+
+function changeBackground() {
+    document.body.style.backgroundColor = "red"
+}
+
+function logInput() {
+    const inputValue = document.getElementById("myInput").value;
+    console.log(inputValue)
+}
+
+function changeBox() {
+    const box = document.getElementById("myBox");
+    box.style.backgroundColor = "green";
+    box.style.width = "150px";
+    box.style.height = "150px";
+}
